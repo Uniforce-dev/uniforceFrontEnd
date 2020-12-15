9412b0d6cc8531d1789215648c9de256d82d954c
intouchsophie <matali@rossinante.localdomain>
2020-12-15 16:19:04 +0100

    add streamers photos & infos

--- src/datas/gamersInfos.js
+++ src/datas/gamersInfos.js
@@ -1,68 +1,124 @@
 const gamersInfos = [
     {
         id: 1,
-        pseudo: "Audran",
-        firstname: "Audran",
-        lastname: "Busvelle",
-        age: 19,
-        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
-        favGame: "Minecraft",
-        typeGame: "FPS",
-        motto:"Try hard and chill."
+        pseudo: "NACEAS",
+        nickname: "Kiki",
+        firstname: "Kiliann",
+        lastname: "none",
+        age: 23,
+        description: "Il est fort à Call Of, Okami, Zelda. A part jouer, il aime promener son chien, l'occulte, boire des bières!",
+        favGame: "Call Of, Okami, Zelda",
+        photo:'Kiliann.jpg'
     },
     {
         id: 2,
-        pseudo: "Azzek",
-        firstname: "Valentin",
-        lastname: "Rio",
+        pseudo: "SULI SQZ",
+        nickname: "Suli",
+        firstname: "Sulian",
+        lastname: "none",
         age: 19,
-        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
-        favGame: "Fortnite",
-        typeGame: "FPS, TPS",
-        motto:"Try hard, perf."
+        description: "Il est fort à Apex, Overwatch, TFT. A part jouer, il aime faire la fête et s'amuser avec ses amis!",
+        favGame: "Apex, Overwatch, TFT",
+        photo:'Sulian.jpg'
     },
     {
         id: 3,
-        pseudo: "Mashtyx",
-        firstname: "Léo",
-        lastname: "Gingat",
+        pseudo: "UXIE",
+        nickname: "Lili",
+        firstname: "Lola",
+        lastname: "none",
         age: 18,
-        favGame: "Rocket League",
-        typeGame: "FPS",
-        palmares: "Open Qualifier RLCS",
-        motto:"Try Hard (High tier), perf, chill."
+        description: "Elle est forte à LOL, Celest et Hades. A part jouer, elle aime dormir et faire les boutiques",
+        favGame: "LOL, Celest et Hades",
+        photo:'Lola.jpg'
     },
     {
         id: 4,
-        pseudo: "Fury",
-        firstname: "Alexis",
-        lastname: "Renouard",
+        pseudo: "BEHLOULOPHILE",
+        nickname: "Théophile",
+        firstname: "Behloulophile",
+        lastname: "none",
         age: 21,
-        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
-        favGame: "Heartgstone",
-        typeGame: "Hearthstone, Overwatch, Heroes of the storm",
-        motto:"Try Hard (High tier), perf, chill."
+        description: "Il est fort à Fifa, Call of et Assassins Creed. A part jouer, il aime glander, sortir avec ses potes et son chien",
+        favGame: "Fifa, Call of et Assassins Creed",
+        photo:'Theophile.jpg'
     },
     {
         id: 5,
-        pseudo: "Garcharoth",
-        firstname: "Gweltaz ",
-        lastname: "Bréard",
-        age: 29,
-        favGame: "Overwatch, Pokémon, Jeux Nintendo",
-        typeGame: "FPS, TPS, VSF, RPG, Action, Aventure",
-        motto:"Tryhard ou casu selon les jeux"
-    },
-    {
-        id: 6,
-        pseudo: "HiTnAEker",
-        firstname: "Vincent",
-        lastname: "Ferraton",
-        age: 22,
-        favGame: "OSU!",
-        typeGame: "Tous",
-        motto:"Chill "
+        pseudo: "WARO",
+        nickname: "Waro",
+        firstname: "Lucas",
+        lastname: "none",
+        age: 19,
+        description: "Il est fort à Rocket League, Call of et Rogue Company. A part jouer, il aime le sport, regarder des séries et des films",
+        favGame: "Rocket League, Call of et Rogue Company",
+        photo:'Lucas.jpg'
     }
+    
+    // {
+    //     id: 1,
+    //     pseudo: "Kiliann",
+    //     firstname: "Audran",
+    //     lastname: "Busvelle",
+    //     age: 19,
+    //     // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
+    //     favGame: "Minecraft",
+    //     typeGame: "FPS",
+    //     motto:"Try hard and chill."
+    // },
+    // {
+    //     id: 2,
+    //     pseudo: "Azzek",
+    //     firstname: "Valentin",
+    //     lastname: "Rio",
+    //     age: 19,
+    //     // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
+    //     favGame: "Fortnite",
+    //     typeGame: "FPS, TPS",
+    //     motto:"Try hard, perf."
+    // },
+    // {
+    //     id: 3,
+    //     pseudo: "Mashtyx",
+    //     firstname: "Léo",
+    //     lastname: "Gingat",
+    //     age: 18,
+    //     favGame: "Rocket League",
+    //     typeGame: "FPS",
+    //     palmares: "Open Qualifier RLCS",
+    //     motto:"Try Hard (High tier), perf, chill."
+    // },
+    // {
+    //     id: 4,
+    //     pseudo: "Fury",
+    //     firstname: "Alexis",
+    //     lastname: "Renouard",
+    //     age: 21,
+    //     // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida lobortis lectus, eu placerat magna dapibus non. Cras nec lacus pretium, sollicitudin ante id, pretium justo. Maecenas tincidunt semper tortor vitae rhoncus. Nam ut dignissim sem. Cras congue ligula vitae consectetur porttitor. Donec augue justo, congue a urna id, lacinia hendrerit nisi. Quisque sit amet lacus metus. ",
+    //     favGame: "Heartgstone",
+    //     typeGame: "Hearthstone, Overwatch, Heroes of the storm",
+    //     motto:"Try Hard (High tier), perf, chill."
+    // },
+    // {
+    //     id: 5,
+    //     pseudo: "Garcharoth",
+    //     firstname: "Gweltaz ",
+    //     lastname: "Bréard",
+    //     age: 29,
+    //     favGame: "Overwatch, Pokémon, Jeux Nintendo",
+    //     typeGame: "FPS, TPS, VSF, RPG, Action, Aventure",
+    //     motto:"Tryhard ou casu selon les jeux"
+    // },
+    // {
+    //     id: 6,
+    //     pseudo: "HiTnAEker",
+    //     firstname: "Vincent",
+    //     lastname: "Ferraton",
+    //     age: 22,
+    //     favGame: "OSU!",
+    //     typeGame: "Tous",
+    //     motto:"Chill "
+    // }
 ]
 
 export default gamersInfos
