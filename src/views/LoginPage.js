d2eaa2573cace41ab9f0a8abc41221f5e28f3c0e
segoleneimie <segolene.vincent@imie.fr>
2021-01-06 10:32:44 +0100

    mise en place  des champs de connexion

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -5,7 +5,7 @@ class LoginPage extends Component {
     
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {name: '', mdp: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,28 +13,32 @@ class LoginPage extends Component {
 
   handleChange(event) {
     console.log("handle change")
-    this.setState({value: event.target.value});
+    this.setState({name: event.target.name, mdp: event.target.mdp});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.name +' ' + 
+    'password : ' + this.state.mdp);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label for="name">
-          Name:
-          <input id="name" type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label for="mdp">
-          Mot de passe:
-          <input id="mdp" type="password" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label for="name">
+            Name:
+            <input id="name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <label for="mdp">
+            Mot de passe:
+            <input id="mdp" name="mdp" type="password" value={this.state.mdp} onChange={this.handleChange} />
+          </label>
+          
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      
     );
   }
 
